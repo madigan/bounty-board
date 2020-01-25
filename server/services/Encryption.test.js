3f002e2588f217aa01c999db5e66d6c24b24efba
John Lynn <john@otter.tech>
2020-01-24 17:46:18 -0800

    Clean up unit test.

--- server/services/Encryption.test.js
+++ server/services/Encryption.test.js
@@ -1,6 +1,8 @@
 const Encryption = require('./Encryption');
 
-test("Encryption encrypts a message", () => {
-    const service = new Encryption();
-    expect(service.encrypt("Hello world! How r u?")).toBe("18a645ef2d8af457908830f4c2fe6d7535c49d12bbc3169d112dc397d0237537");
+describe("Encryption", () => {
+    test("Encryption encrypts a message", () => {
+        const service = new Encryption();
+        expect(service.encrypt("Hello world! How r u?")).toBe("18a645ef2d8af457908830f4c2fe6d7535c49d12bbc3169d112dc397d0237537");
+    });
 });
